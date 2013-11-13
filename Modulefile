85eee11ecb355253b314f0de60205421503ac1a9
Bennett Samowich <bennett@foolean.org>
2013-11-13 11:44:52 -0500

    Added basic module documentation

---
+++ Modulefile
@@ -0,0 +1,22 @@
+name    'foolean-puppet'
+version '0.0.1'
+
+author  'bennett@foolean.org'
+summary 'Manage puppet agents and master'
+description 'Manage puppet agents as well as the puppetmaster.  This module
+also provides the ability to manage multiple environments for the purposes of
+providing puppet services to disparate sites resulting in CM as a service.'
+
+license 'Copyright (c) 2013 Foolean.org
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.'
--- manifests/init.pp
+++ manifests/init.pp
@@ -5,33 +5,57 @@
 #
 # === Parameters
 #
-# [*mode*]
-#
-# [*sites*]
-#
 # [*agent*]
+#   Agent configuration block
 #
 # [*main*]
+#   Main configuration block
 #
 # [*master*]
+#   Master configuration block
+#
+# [*mode*]
+#   The mode of operation the module is to run as.  Possible values
+#   are 'agent' and 'master'.   The default is 'agent'
+#
+# [*sites*]
+#   Hash-of-hashes that denotes the sites the puppetmaster will
+#   provide service for.
+#
+# [*user*]
+#   User configuration block
 #
 # === Variables
 #
-# [*sys_user*]
-#
-# [*sys_group*]
-#
-# [*puppet_user*]
-#
-# [*puppet_group*]
+# [*confdir*]
+#   The main Puppet configuration directory. The default for this setting is
+#   calculated based on the user. If the process is running as root or the
+#   user that Puppet is supposed to run as, it defaults to a system directory,
+#   but if it’s running as any other user, it defaults to being in the user’s
+#   home directory.
 #
 # [*client_packages*]
+#   Array of packages that 'agents' need.
 #
 # [*dev_packages*]
+#   Puppet developer packages to be installed.
 #
-# [*confdir*]
+# [*puppet_group*]
+#   The puppet user's group name.
+#
+# [*puppet_user*]
+#   The user that puppet will operate as, typcially 'puppet'.
+#
+# [*sys_group*]
+#   The sytem user's group name, typically root but can be different
+#   depending on the OS.
+#
+# [*sys_user*]
+#   The sytem username, typically root but can be different
+#   depending on the OS.
 #
 # [*vardir*]
+#   Where Puppet stores dynamic and growing data. 
 #
 # === Supported Operating Systems
 #
@@ -48,19 +72,19 @@
 #
 # === Copyright
 #
-# Copyright (c) 2013 Foolean.org
+#   Copyright (c) 2013 Foolean.org
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 #
 class puppet (
     $mode   = 'agent',
